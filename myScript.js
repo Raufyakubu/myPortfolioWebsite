39463c3c90b2a90385ea8a5e3c5162cb68af4488
Raufyakubu <raufyakubu@gmail.com>
2024-12-13 18:22:43 +0000

    A new logo was added and some skills were updated.

--- myScript.js
+++ myScript.js
@@ -23,22 +23,3 @@ var sidemeu = document.getElementById("sidemenu");
     function closemenu(){
         sidemeu.style.right = "-200px";
     }
-
-/*Contact form: the details of the user is sent to a google sheet file*/ 
-  const scriptURL = 'https://script.google.com/macros/s/AKfycbwOXCVNKVR6yi2eXDZLqCvx_V7ia44E8gbqAoBZw1X_ev-ra_a7TH1ofXUlVe2ecZC6/exec'
-  const form = document.forms['submit-to-google-sheet']
-  const popUp = document.getElementById('popUp')
-
-  form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => {
-        popUp.innerHTML ="Message sent sucessfully";
-        setTimeout(function(){
-            popUp.innerHTML = ""
-        },3000)
-        form.reset()
-      })
-      .catch(error => console.error('Error!', error.message))
-  })
-
